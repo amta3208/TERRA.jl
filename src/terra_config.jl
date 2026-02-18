dd71feacb5cba870e829cc217849ad2f7ce163d9
Amin Taziny <amta3208@colorado.edu>
2026-02-18 15:00:46 -0700

    finalize src/io layout

--- src/terra_config.jl
+++ src/terra_config.jl
@@ -7,5 +7,8 @@ including parameter validation, default values, and input file generation.
 
 include("config/types.jl")
 include("config/validation.jl")
+include("io/prob_setup_writer.jl")
+include("io/sources_setup_writer.jl")
+include("io/tau_scaling_writer.jl")
 include("io/input_generation.jl")
 include("config/conversions.jl")
